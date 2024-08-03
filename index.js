40a8370c22e4360b045df8199a3339f57eeb81f0
unknown <maria@gmail.com>
2024-08-03 10:24:21 +0300

    Make a Select dynamic with data retrived from API

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function getStudents(){
     fetch('https://api-n1hn.onrender.com/students')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(data=> {
+        var select_users = document.getElementById('select_users');
+        data.forEach(st => {
+            var opt = document.createElement('option');
+            opt.innerHTML = st.name;
+            select_users.appendChild(opt);
+        });
+      })
 }
 getStudents();
